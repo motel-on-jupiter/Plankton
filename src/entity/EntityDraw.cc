ca2fd0c56c59c59e12a39ad59c8674b9d20115ea
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-08-30 13:41:50 +0900

    エンティティ描画MixInクラスを定義する

--- src/entity/EntityDraw.cc
+++ src/entity/EntityDraw.cc
@@ -1,5 +1,59 @@
 /**
  * Copyright (C) 2014 The Motel on Jupiter
  */
-#include "entity/EntityDraw.h"
+#include "EntityDraw.h"
 #include "entity/BaseEntity.h"
+#include "util/logging/Logger.h"
+#include "util/wrapper/glgraphics_wrap.h"
+
+SphereEntityDraw::SphereEntityDraw(BaseEntity &entity, const glm::vec3 &ambient,
+                                   const glm::vec3 &difusse, const glm::vec3 &specular,
+                                   float shiness, int slices, int stacks)
+: BaseEntityDraw(entity), quadric_(nullptr), colors_(), shiness_(shiness), slices_(slices),
+  stacks_(stacks) {
+  colors_.push_back(ambient);
+  colors_.push_back(difusse);
+  colors_.push_back(specular);
+}
+
+SphereEntityDraw::SphereEntityDraw(BaseEntity &entity, const glm::vec3 &ambient_and_difusse,
+                                   const glm::vec3 &specular, float shiness, int slices,
+                                   int stacks)
+: BaseEntityDraw(entity), quadric_(nullptr), colors_(), shiness_(shiness), slices_(slices),
+  stacks_(stacks) {
+  colors_.push_back(ambient_and_difusse);
+  colors_.push_back(specular);
+}
+
+int SphereEntityDraw::Initialize() {
+  quadric_ = gluNewQuadric();
+  if (quadric_ == nullptr) {
+    LOGGER.Error("Failed to create quadric object");
+    return -1;
+  }
+  return 0;
+}
+
+void SphereEntityDraw::Finalize() {
+  gluDeleteQuadric(quadric_);
+  quadric_ = nullptr;
+}
+
+void SphereEntityDraw::Draw() {
+  if (colors_.size() == 2) {
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, glm::value_ptr(colors_[0]));
+    glMaterialfv(GL_FRONT, GL_SPECULAR, glm::value_ptr(colors_[1]));
+  } else if (colors_.size() == 3) {
+    glMaterialfv(GL_FRONT, GL_AMBIENT, glm::value_ptr(colors_[0]));
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, glm::value_ptr(colors_[1]));
+    glMaterialfv(GL_FRONT, GL_SPECULAR, glm::value_ptr(colors_[2]));
+  } else {
+    assert(false);
+  }
+  glMaterialfv(GL_FRONT, GL_SHININESS, &shiness_);
+
+  glPushMatrix();
+  glMultMatrixf(glm::value_ptr(glm::translate(entity().pos())));
+  gluSphere(quadric_, entity().scale().x * 0.5f, slices_, stacks_);
+  glPopMatrix();
+}
--- src/entity/EntityDraw.h
+++ src/entity/EntityDraw.h
@@ -4,14 +4,38 @@
 #ifndef ENTITYDRAW_H_
 #define ENTITYDRAW_H_
 
+#include <vector>
 #include "entity/BaseEntity.h"
+#include "util/wrapper/glm_wrap.h"
 
-class EntityDraw : public EntityMixIn {
+class BaseEntityDraw : public EntityMixIn {
  public:
-  EntityDraw(BaseEntity &entity) : EntityMixIn(entity) {}
-  virtual ~EntityDraw() {}
+  BaseEntityDraw(BaseEntity &entity) : EntityMixIn(entity) {}
+  virtual ~BaseEntityDraw() {}
 
+  virtual int Initialize() = 0;
+  virtual void Finalize() = 0;
   virtual void Draw() = 0;
+};
+
+class SphereEntityDraw : public BaseEntityDraw {
+ public:
+  SphereEntityDraw(BaseEntity &entity, const glm::vec3 &ambient, const glm::vec3 &difusse,
+                   const glm::vec3 &specular, float shiness, int slices, int stacks);
+  SphereEntityDraw(BaseEntity &entity, const glm::vec3 &ambient_and_difusse, const glm::vec3 &specular,
+                   float shiness, int slices, int stacks);
+  virtual ~SphereEntityDraw() {}
+
+  virtual int Initialize();
+  virtual void Finalize();
+  virtual void Draw();
+
+ private:
+  GLUquadric *quadric_;
+  std::vector<glm::vec3> colors_;
+  float shiness_;
+  int slices_;
+  int stacks_;
 };
 
 #endif /* ENTITYDRAW_H_ */
